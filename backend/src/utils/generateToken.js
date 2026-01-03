22e06c9fba9705ca292ae9faa1a872626adf6213
Rinsharino6b6 <rinsharinsha73650@gmail.com>
2026-01-03 20:48:11 +0530

    Update generateToken to include user role

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -1,7 +1,13 @@
 const jwt = require("jsonwebtoken");
 
-const generateToken = (userId) => {
-  return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
+const generateToken = (user) => {
+  return jwt.sign(
+    {
+      id: user._id,
+      role: user.role,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" }
+  );
 };
-
 module.exports = generateToken;
