f448407fff551f0e20b16fcf6d18e5b882d88531
Rinsharino6b6 <rinsharinsha73650@gmail.com>
2026-01-04 10:55:04 +0530

    Delete backend/src/comment.js

--- backend/src/comment.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const { addComment, getComments } = require("../controllers/commentController");
-
-router.post("/", addComment);
-router.get("/:id", getComments);
-
-module.exports = router;
